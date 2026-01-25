51a8fe54ded6b94a3cbaf6dcc7e215acf0ca976d
Volodymyr Petrytsya <Vpetrytsia@gmail.com>
2026-01-24 20:55:26 -0500

    db updated

--- prisma/migrations/20260124165751_/migration.sql
+++ prisma/migrations/20260124165751_/migration.sql
@@ -1,5 +1,17 @@
--- RenameIndex
-ALTER INDEX "email_log_digest_unique" RENAME TO "EmailLog_parishId_weekId_userId_type_key";
+-- RenameIndex (safe / idempotent)
+DO $$
+BEGIN
+  IF to_regclass('public.email_log_digest_unique') IS NOT NULL THEN
+    ALTER INDEX public.email_log_digest_unique
+      RENAME TO "EmailLog_parishId_weekId_userId_type_key";
+  END IF;
+END $$;
 
--- RenameIndex
-ALTER INDEX "email_log_join_request_unique" RENAME TO "EmailLog_joinRequestId_toEmail_type_key";
+-- RenameIndex (safe / idempotent)
+DO $$
+BEGIN
+  IF to_regclass('public.email_log_join_request_unique') IS NOT NULL THEN
+    ALTER INDEX public.email_log_join_request_unique
+      RENAME TO "EmailLog_joinRequestId_toEmail_type_key";
+  END IF;
+END $$;
