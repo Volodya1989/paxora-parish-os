a68ec0309cbd93b3ca1aadb3b1940d557751e43f
Volodymyr Petrytsya <Vpetrytsia@gmail.com>
2026-01-27 20:10:41 -0500

    a-015_1

--- prisma/migrations/20260128001956_tasks/migration.sql
+++ prisma/migrations/20260128001956_tasks/migration.sql
@@ -1,8 +1,20 @@
--- AlterTable
-ALTER TABLE "Task" ALTER COLUMN "updatedAt" DROP DEFAULT;
+DO $$
+BEGIN
+  -- If the column is camelCase
+  IF EXISTS (
+    SELECT 1
+    FROM information_schema.columns
+    WHERE table_name = 'Task' AND column_name = 'updatedAt'
+  ) THEN
+    EXECUTE 'ALTER TABLE "Task" ALTER COLUMN "updatedAt" DROP DEFAULT';
+  END IF;
 
--- RenameIndex
-ALTER INDEX "email_log_digest_unique" RENAME TO "EmailLog_parishId_weekId_userId_type_key";
-
--- RenameIndex
-ALTER INDEX "email_log_join_request_unique" RENAME TO "EmailLog_joinRequestId_toEmail_type_key";
+  -- If the column is snake_case
+  IF EXISTS (
+    SELECT 1
+    FROM information_schema.columns
+    WHERE table_name = 'Task' AND column_name = 'updated_at'
+  ) THEN
+    EXECUTE 'ALTER TABLE "Task" ALTER COLUMN "updated_at" DROP DEFAULT';
+  END IF;
+END $$;
