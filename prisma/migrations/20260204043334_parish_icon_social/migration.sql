11c2fd54f785f2005c79c2f2f720a0b5480d12bb
Volodymyr Petrytsya <Vpetrytsia@gmail.com>
2026-02-04 00:08:22 -0500

    db_1

--- prisma/migrations/20260204043334_parish_icon_social/migration.sql
+++ prisma/migrations/20260204043334_parish_icon_social/migration.sql
@@ -5,8 +5,45 @@
 -- multiple migrations, each migration adding only one value to
 -- the enum.
 
+-- SAFETY: Ensure the enum exists in DB (some environments may not have it yet).
+-- We create it with the "base" values (the ones that existed BEFORE this migration).
+-- Then we add the new values one-by-one, ignoring duplicates.
 
-ALTER TYPE "ParishIcon" ADD VALUE 'FACEBOOK';
-ALTER TYPE "ParishIcon" ADD VALUE 'YOUTUBE';
-ALTER TYPE "ParishIcon" ADD VALUE 'PRAYER';
-ALTER TYPE "ParishIcon" ADD VALUE 'NEWS';
+DO $$ BEGIN
+  CREATE TYPE "ParishIcon" AS ENUM (
+    'BULLETIN',
+    'MASS_TIMES',
+    'CONFESSION',
+    'WEBSITE',
+    'CALENDAR',
+    'READINGS',
+    'GIVING',
+    'CONTACT'
+  );
+EXCEPTION
+  WHEN duplicate_object THEN NULL;
+END $$;
+
+DO $$ BEGIN
+  ALTER TYPE "ParishIcon" ADD VALUE 'FACEBOOK';
+EXCEPTION
+  WHEN duplicate_object THEN NULL;
+END $$;
+
+DO $$ BEGIN
+  ALTER TYPE "ParishIcon" ADD VALUE 'YOUTUBE';
+EXCEPTION
+  WHEN duplicate_object THEN NULL;
+END $$;
+
+DO $$ BEGIN
+  ALTER TYPE "ParishIcon" ADD VALUE 'PRAYER';
+EXCEPTION
+  WHEN duplicate_object THEN NULL;
+END $$;
+
+DO $$ BEGIN
+  ALTER TYPE "ParishIcon" ADD VALUE 'NEWS';
+EXCEPTION
+  WHEN duplicate_object THEN NULL;
+END $$;
